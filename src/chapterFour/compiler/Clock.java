af4f8f6cdc5470f3c1a5aa659e467624b04c614e
anupam <anupamdas0376@gmail.com>
2022-08-13 22:21:36 +0600

    Added Chapter Four Exercise

---
+++ src/chapterFour/compiler/Clock.java
@@ -0,0 +1,12 @@
+package chapterFour.compiler;
+
+public class Clock {
+        String time;
+        void setTime(String t) {
+            time = t;
+        }
+        String getTime() {
+            return time;
+        }
+
+}
---
+++ src/chapterFour/poolPuzzle/Value.java
@@ -0,0 +1,12 @@
+package chapterFour.poolPuzzle;
+
+public class Value {
+    int intValue;
+    public int doStuff(int factor) {
+        if (intValue > 100) {
+            return intValue * factor;
+        } else {
+            return intValue * (5 - factor);
+        }
+    }
+}
